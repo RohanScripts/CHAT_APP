dd17bf59d5a34b2a539fb2ff7273465e21f13b4e
RohanScripts <shelarrohan1810@gmail.com>
2024-04-26 22:28:56 +0530

    Update Chat component UI to include emoji picker functionality

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -4,6 +4,13 @@ import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
   const [openEmoji, setOpenEmoji] = useState(false);
+  const [text, setText] = useState("");
+
+  console.log(text);
+
+  const handelEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+  };
 
   return (
     <div className="chat">
@@ -28,10 +35,28 @@ const Chat = () => {
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input type="text" name="" id="" placeholder="type a message..." />
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          name=""
+          id=""
+          placeholder="type a message..."
+        />
         <div className="emoji">
-          <img src="./emoji.png" alt="" />
-          <EmojiPicker></EmojiPicker>
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpenEmoji((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker
+              height={350}
+              width={300}
+              onEmojiClick={handelEmoji}
+              open={openEmoji}
+            ></EmojiPicker>
+          </div>
         </div>
         <button className="send-button">Send</button>
       </div>
