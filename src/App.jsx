ec8cfc1e8f601dbce2d9784f7b221f22441cc84e
RohanScripts <shelarrohan1810@gmail.com>
2024-04-29 20:01:34 +0530

    Add Login component and update App.jsx to include login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import List from "./components/list/List";
 import Chat from "./components/chat/Chat";
 import Details from "./components/details/Details";
+import Login from "./components/login/Login";
 
 const App = () => {
+  const user = false;
+
   return (
     <div className="container">
-      <List></List>
-      <Chat></Chat>
-      <Details></Details>
+      {user ? (
+        <>
+          <List></List>
+          <Chat></Chat>
+          <Details></Details>
+        </>
+      ) : (
+        <Login></Login>
+      )}
     </div>
   );
 };
