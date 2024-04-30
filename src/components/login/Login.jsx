0cc4252e8ccd2c9d6964ccad42dc29cc0235cb7a
RohanScripts <shelarrohan1810@gmail.com>
2024-04-30 22:10:13 +0530

    Refactor Login component UI to include login and sign in forms

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import "./LoginCss.css";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="login">
+      <div className="item">
+        <h2>Welcome Back,</h2>
+        <form>
+          <input type="email" name="email" placeholder="email..." id="" />
+          <input type="passoword" name="password" placeholder="password" />
+          <button>Log in</button>
+        </form>
+      </div>
+      <div className="separator"></div>
+      <div className="item">
+        <h2>Create an account</h2>
+        <form>
+          <input type="text" name="username" placeholder="username..." id="" />
+          <input type="email" name="email" placeholder="email..." id="" />
+          <input type="passoword" name="password" placeholder="password" />
+          <button>Sign in</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
