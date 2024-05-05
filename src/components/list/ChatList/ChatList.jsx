186379408ab77f88670dfcbac0e2a0b9422a2afd
RohanScripts <shelarrohan1810@gmail.com>
2024-05-05 23:55:53 +0530

    refactor: Update ChatList component to fetch and display user chats dynamically

--- src/components/list/ChatList/ChatList.jsx
+++ src/components/list/ChatList/ChatList.jsx
@@ -1,9 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ChatListCss.css";
 import AddUser from "../../add-user/addUser";
+import { useUserStore } from "../../../library/userStore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../library/firebase";
 
 const ChatList = () => {
   const [showMinusIcon, setShowMinusIcon] = useState(false);
+
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userChats", currentUser.id),
+      async (res) => {
+        const items = res.data().chats;
+
+        const promises = items.map(async (eachItem) => {
+          const userDocRef = doc(db, "users", eachItem.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          const user = userDocSnap.data();
+
+          return { ...eachItem, user };
+        });
+
+        const chatData = await Promise.all(promises);
+
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
+
+  console.log(chats);
 
   return (
     <div className="chat-list">
@@ -18,55 +53,16 @@ const ChatList = () => {
           className="plus"
         />
       </div>
-      <div className="items">
-        <img src="./avatar.png" alt="" />
-        <div className="text">
-          <span>Rohan Shelar</span>
-          <p>cold coco</p>
-        </div>
-      </div>
-      <div className="items">
-        <img src="./avatar.png" alt="" />
-        <div className="text">
-          <span>Rohan Shelar</span>
-          <p>cold coco</p>
-        </div>
-      </div>
-      <div className="items">
-        <img src="./avatar.png" alt="" />
-        <div className="text">
-          <span>Rohan Shelar</span>
-          <p>cold coco</p>
-        </div>
-      </div>
-      <div className="items">
-        <img src="./avatar.png" alt="" />
-        <div className="text">
-          <span>Rohan Shelar</span>
-          <p>cold coco</p>
-        </div>
-      </div>
-      <div className="items">
-        <img src="./avatar.png" alt="" />
-        <div className="text">
-          <span>Rohan Shelar</span>
-          <p>cold coco</p>
-        </div>
-      </div>
-      <div className="items">
-        <img src="./avatar.png" alt="" />
-        <div className="text">
-          <span>Rohan Shelar</span>
-          <p>cold coco</p>
-        </div>
-      </div>
-      <div className="items">
-        <img src="./avatar.png" alt="" />
-        <div className="text">
-          <span>Rohan Shelar</span>
-          <p>cold coco</p>
-        </div>
-      </div>
+
+      {chats.map((eachChat) => {
+        <div className="items" key={eachChat.chatId}>
+          <img src="./avatar.png" alt="" />
+          <div className="text">
+            <span>Rohan Shelar</span>
+            <p>cold coco</p>
+          </div>
+        </div>;
+      })}
       {showMinusIcon ? <AddUser></AddUser> : ""}
     </div>
   );
